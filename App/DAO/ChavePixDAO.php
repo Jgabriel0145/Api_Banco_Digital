62c072414b76c71cf0fa7b83e2ead061832518d6
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-04-11 15:31:40 -0300

    Criação das classes.

---
+++ App/DAO/ChavePixDAO.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\DAO;
+
+class ChavePixDAO extends DAO
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
---
+++ App/Model/ChavePixModel.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Model;
+
+class ChavePixModel extends Model
+{
+    
+}
