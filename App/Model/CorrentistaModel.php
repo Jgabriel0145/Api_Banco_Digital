a2aebee310ec937e1ef1660de03295b792d73413
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-07-27 15:53:34 -0300

    Criação de contas.

--- App/Model/CorrentistaModel.php
+++ App/Model/CorrentistaModel.php
@@ -5,14 +5,43 @@ namespace App\Model;
 use Exception;
 
 use App\DAO\CorrentistaDAO;
+use App\DAO\ContaDAO;
 
 class CorrentistaModel extends Model
 {
-    public $id, $nome, $cpf, $data_nasc, $senha, $email, $data_cadastro;
+    public $id, $nome, $cpf, $data_nasc, $senha, $email, $data_cadastro; //correntista
+    public $rows_contas; //conta
 
     public function Save() : ?CorrentistaModel
     {
-        return (new CorrentistaDAO())->Save($this);        
+        $model_completa = (new CorrentistaDAO())->Save($this);
+
+        if ($model_completa->id != null)
+        {
+            $conta_dao = new ContaDAO();
+
+            //conta corrente
+            $conta_corrente = new ContaModel();
+            $conta_corrente->id_correntista = $model_completa->id;
+            $conta_corrente->saldo = 0;
+            $conta_corrente->limite = 100;
+            $conta_corrente->tipo = 'C';
+            $conta_corrente = $conta_dao->Insert($conta_corrente);
+
+            $model_completa->rows_contas[] = $conta_corrente;
+
+            //conta poupança
+            $conta_poupanca = new ContaModel();
+            $conta_poupanca->id_correntista = $model_completa->id;
+            $conta_poupanca->saldo = 0;
+            $conta_poupanca->limite = 0;
+            $conta_poupanca->tipo = 'P';
+            $conta_poupanca = $conta_dao->Insert($conta_poupanca);
+
+            $model_completa->rows_contas[] = $conta_poupanca;
+        }
+
+        return $model_completa;
     }
 
     public function GetAllRows(string $query = null)
